43f57ed1717ee81d2c734e4dc6b057e9c5d43ed1
Setsushin <x-zang@unext.jp>
2024-11-25 18:15:48 +0900

    update de-cli-tool to v0.1-beta

--- de-cli-tool.rb
+++ de-cli-tool.rb
@@ -1,9 +1,9 @@
 class DeCliTool < Formula
 	desc "A Python-based CLI tool for U-NEXT DE members"
 	homepage "https://github.com/u-next/de-cli-tool"
-	url "https://github.com/u-next/de-cli-tool/releases/download/v0.0.1t/de-cli-tool"
-	version "0.0.1t"
-	sha256 "5ef595fae1aca2edafccd8e39d067908b66647daa8473d912285f9b6b24ceba8"
+	url "https://github.com/u-next/de-cli-tool/releases/download/v0.1-beta/de-cli-tool"
+	version "v0.1-beta"
+	sha256 "f6666304c0e8f24bd659745f57c60727e557badeb952821db4fcd3f7f7753450"
   def install
 		bin.install "de-cli-tool" => "de-cli-tool"
 	end
